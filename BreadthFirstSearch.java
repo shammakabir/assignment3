7458b8017014a927872317eeeb800060765b885b
shammakabir <shammakabir@utexas.edu>
2016-09-25 18:09:09 -0500

    Update BreadthFirstSearch.java

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -1,29 +1,23 @@
 package assignment3;
 
-import java.util.ArrayList;
+
+import java.util.LinkedList;
 import java.util.Set;
 
-class WordNode {  //why not make this a class?
-	String word;
-	int length = 0;
-	
-	public WordNode(String w, int l) {
-		word = w;
-		length = l;
-	}
-}
+
 
 public class BreadthFirstSearch {
-	public int breadthFirstSearch(String start, String end, Set<String> dictionary) { // why isnt this a static method, needs to return an array list
-		ArrayList<WordNode> storage = new ArrayList<WordNode>();
-		storage.add(new WordNode(start,0));
-		int length = 0; // what is this used for?
+
+	public static void breadthFirstSearch(String start, String end, Set<String> dictionary) {
+		LinkedList<WordNode> storage = new LinkedList<WordNode>();
+		storage.add(new WordNode(start, 0));
+		if (start.equals(end)) { //if start and end words are the same
+			return;
+			}
 		while (storage.isEmpty() != true) {
-			if (start.equals(end)) { //if start and end words are the same
-				return 0;
-		}
-			
-			char[] check = start.toCharArray();
+			WordNode newWord = storage.remove();
+			String newWordCheck = newWord.word;
+			char[] check = newWordCheck.toCharArray();
 			for (int i = 0; i < check.length; i++) { //changing every letter of start word 
 				for (char j = 'a'; j <= 'z'; j++){ //changing each letter from a - z
 					char temp = check[i];
@@ -32,18 +26,14 @@ public class BreadthFirstSearch {
 					}
 					String checkWord = new String(check);
 					if (dictionary.contains(checkWord)) {
-						length += 1;
-						storage.add(new WordNode(checkWord, length)); // add word to the queue
-						dictionary.remove(checkWord); //word has been visited THIS KILLS THE DICTIONARY
+						storage.add(new WordNode(checkWord, newWord.rung + 1)); // add word to the queue
 					}
 					
 					check[i] = temp; //put the original letter back
 				}
 			}
 			
-			
 		}
-		return length;
 	}
 
 }

d4ed60bf68d91322e8b752dfe7fc77b138293131
Jtk764 <jknight911@utexas.edu>
2016-09-25 15:02:24 -0500

    comments on BFS

--- BreadthFirstSearch.java
+++ BreadthFirstSearch.java
@@ -3,7 +3,7 @@ package assignment3;
 import java.util.ArrayList;
 import java.util.Set;
 
-class WordNode {
+class WordNode {  //why not make this a class?
 	String word;
 	int length = 0;
 	
@@ -14,10 +14,10 @@ class WordNode {
 }
 
 public class BreadthFirstSearch {
-	public int breadthFirstSearch(String start, String end, Set<String> dictionary) {
+	public int breadthFirstSearch(String start, String end, Set<String> dictionary) { // why isnt this a static method, needs to return an array list
 		ArrayList<WordNode> storage = new ArrayList<WordNode>();
 		storage.add(new WordNode(start,0));
-		int length = 0;
+		int length = 0; // what is this used for?
 		while (storage.isEmpty() != true) {
 			if (start.equals(end)) { //if start and end words are the same
 				return 0;
@@ -34,7 +34,7 @@ public class BreadthFirstSearch {
 					if (dictionary.contains(checkWord)) {
 						length += 1;
 						storage.add(new WordNode(checkWord, length)); // add word to the queue
-						dictionary.remove(checkWord); //word has been visited
+						dictionary.remove(checkWord); //word has been visited THIS KILLS THE DICTIONARY
 					}
 					
 					check[i] = temp; //put the original letter back
